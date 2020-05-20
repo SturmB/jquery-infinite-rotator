2cc1a099126cb13d803bc2c09b0a50a5d65d44a0
Chris McGee <chris@chrismcgee.info>
2020-05-20 15:41:54 -0400

    Ugh

--- js/infinite-rotator.js
+++ js/infinite-rotator.js
@@ -11,7 +11,6 @@
 // **********************************************************************
 
 $(window).load(function() {
-<<<<<<< HEAD
   //start after HTML, images have loaded
 
   // Enable the passage of the 'this' object through the JavaScript timers
@@ -28,8 +27,8 @@ $(window).load(function() {
     return __nativeST__(
       vCallback instanceof Function
         ? function() {
-          vCallback.apply(oThis, aArgs);
-        }
+            vCallback.apply(oThis, aArgs);
+          }
         : vCallback,
       nDelay
     );
@@ -44,8 +43,8 @@ $(window).load(function() {
     return __nativeSI__(
       vCallback instanceof Function
         ? function() {
-          vCallback.apply(oThis, aArgs);
-        }
+            vCallback.apply(oThis, aArgs);
+          }
         : vCallback,
       nDelay
     );
@@ -101,95 +100,4 @@ $(window).load(function() {
   }; // InfiniteRotator
 
   InfiniteRotator.init();
-=======
-  //start after HTML, images have loaded
-
-  // Enable the passage of the 'this' object through the JavaScript timers
-
-  var __nativeST__ = window.setTimeout,
-    __nativeSI__ = window.setInterval;
-
-  window.setTimeout = function(
-    vCallback,
-    nDelay /*, argumentToPass1, argumentToPass2, etc. */
-  ) {
-    var oThis = this,
-      aArgs = Array.prototype.slice.call(arguments, 2);
-    return __nativeST__(
-      vCallback instanceof Function
-        ? function() {
-          vCallback.apply(oThis, aArgs);
-        }
-        : vCallback,
-      nDelay
-    );
-  };
-
-  window.setInterval = function(
-    vCallback,
-    nDelay /*, argumentToPass1, argumentToPass2, etc. */
-  ) {
-    var oThis = this,
-      aArgs = Array.prototype.slice.call(arguments, 2);
-    return __nativeSI__(
-      vCallback instanceof Function
-        ? function() {
-          vCallback.apply(oThis, aArgs);
-        }
-        : vCallback,
-      nDelay
-    );
-  };
-
-  var InfiniteRotator = {
-    init: function() {
-      $(".rotating-item-group").each(function() {
-        //initial fade-in time (in milliseconds)
-        var initialFadeIn = 1000;
-
-        //interval between items (in milliseconds)
-        var itemInterval = 5000;
-
-        //cross-fade time (in milliseconds)
-        var fadeTime = 2500;
-
-        //count number of items
-        var numberOfItems = $(this).children(".rotating-item").length;
-
-        //set current item
-        var currentItem = 0;
-
-        //show first item
-        $(this)
-          .children(".rotating-item")
-          .eq(currentItem)
-          .fadeIn(initialFadeIn);
-
-        //loop through the items
-        var infiniteLoop = setInterval.call(
-          $(this),
-          function() {
-            $(this)
-              .children(".rotating-item")
-              .eq(currentItem)
-              .fadeOut(fadeTime);
-
-            if (currentItem === numberOfItems - 1) {
-              currentItem = 0;
-            } else {
-              currentItem++;
-            }
-            $(this)
-              .children(".rotating-item")
-              .eq(currentItem)
-              .fadeIn(fadeTime);
-          },
-          itemInterval
-        );
-      }); // $('rotating-item-wrapper').each()
-    } // init
-  }; // InfiniteRotator
-
-  InfiniteRotator.init();
->>>>>>> eaf68bb20b6c98515b24531d3708d6ff17c45738
 });
